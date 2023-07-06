bc89c01f50fffc40bf3092b8fb9b7660fbbf5103
daridev <darideveloper@gmail.com>
2023-07-06 14:13:38 -0600

    feat: initial files

---
+++ __main__.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+from logs import logger
+from scraping_manager.automate import WebScraping
+
+load_dotenv ()
+VAR = os.getenv ("VAR")
+
+def main (): 
+
+
+if __name__ == "__main__":
+    main()
